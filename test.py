94b5c7d5af2c33e067d3ba38b5b670384b9ecd12
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-05-04 10:33:50 +1000

    Complete core CLI algorithimic logic, w/docstrings

--- test.py
+++ test.py
@@ -1,63 +1,63 @@
+
+# Helper to process sales:
 def process_sales() -> list[list]:
   '''
-  Write up docstring
+  Outputs all trasnactions listed within sales.txt file cleanly into a list of lists.
+  
+  Args:
+    No explicit Args, instead function relies on existence of sales.txt file
+  
+  Returns:
+    list[list]: One list containing all transactions which are also stored in a list i.e. sales = [[1,0,0,1],[0,0,2,1]].
   '''
   arr = []
-  with open("sales.txt","r") as file:
-    for transaction in file:
-      # process each transaction:
-      x = transaction.split(",")
-  
-      for y in x:
-        y = y.strip() #helps to remove \n character from final value in each transaction list
-        y = int(y) 
-        arr.append(y) # append all numbers from sales file into arr..
-      
+  try:
+    with open("sales.txt","r") as file:
+      for transaction in file:
+        # process each transaction (i.e. each line):
+        x = transaction.split(",")
+    
+        for y in x:
+          y = y.strip() #helps to remove \n character from final value in each transaction list
+          y = int(y) 
+          arr.append(y) # append all numbers from sales file into arr..
+        
       n = 4
       res=[]
       # break arr into chunks of 4, to get list of lists:
       for i in range(0,len(arr),n):
         res.append(arr[i:i+n])
       
-    return res
+      return res
+  except FileNotFoundError:
+    # TO DO: HANDLE FILE NOT FOUND ERROR PROPERLY
+    print("File not found Error...")
   
-def calc_total(fam_pass_a,fam_pass_b,adult,child):
+# Helper to calculate total revenue:
+# TO DO: SET DEFAULT VALUES OF THESE TO WHAT THEY WERE IN AT1:
+def calc_total(fam_pass:float,adult:float,child:float)-> float:
   '''
-  Write up docstring
+  Outputs total revenue by adding all revenue of all individual transactions.
+  
+  Args:
+    fam_pass (float): User set price of family pass.
+    adult (float): User set price of adult ticket.
+    child (float): User set price of child ticket.
+  
+  Returns:
+    (float): Total revenue for all transactions.  
   '''
   
   sales = process_sales()
-  fam_a=0
-  fam_b=0
+  fam_p=0
   ad=0
   ch=0
   for transaction in sales:
-    fam_a += (fam_pass_a * transaction[0])
-    fam_b += (fam_pass_b * transaction[1])
+    fam_p += (fam_pass * transaction[0])
+    fam_p += (fam_pass * transaction[1])
     ad += (adult * transaction[2])
     ch += (child * transaction[3])
-  return fam_a + fam_b + ad + ch
+  return fam_p + ad + ch
     
-print(f"Total rev for this transaction: ${calc_total(16,16,5,4)}")
-
-'''
-Everything done step by step... not the best implementation in terms of time and space complexity, this will suffer hard under scale,
-but built from logical principles started with this in online python interpreter. This serves basis of entire prorgam:
-
-sales = [[1, 0, 0, 0],[0, 0, 2, 0],[0, 1, 1, 0],[2, 0, 0, 1]] {This is the entire purpose of process_sales functions to get our sales file data looking nice like this}
-
-def calc_total(fam_pass_a,fam_pass_b,adult,child):
-    fam_a=0
-    fam_b=0
-    ad=0
-    ch=0
-    for transaction in sales:
-        fam_a += (fam_pass_a * transaction[0])
-        fam_b += (fam_pass_b * transaction[1])
-        ad += (adult * transaction[2])
-        ch += (child * transaction[3])
-    return fam_a + fam_b + ad + ch
-    
-print(f"Total rev for this transaction: ${calc_total(16,16,5,4)}")
-'''
+print(f"Total rev for this transaction: ${calc_total(18.5,6,4.5)}")
 
