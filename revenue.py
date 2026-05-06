8691ff35201d6904c2a99e7e2aabcab5f66b9b21
Ashutosh Kuinkel <108271619+hejjwheh@users.noreply.github.com>
2026-05-06 20:57:20 +1000

    chore: add comments + docstrings

--- revenue.py
+++ revenue.py
@@ -1,5 +1,21 @@
 '''
-Write up module docstring... Explaining the entire program, serves as a preface.
+revenue.py : CodeTown Public Pool GUI sales-review program.
+
+The purpose of this program is to receive an input for the prices of the family pass tickets, 
+adult tickets and child tickets and calculate the revenue based on those prices and the ticket
+amounts based on the sales.txt file.
+
+The approach taken to solve this problem was to first present the re-sizable GUI to the user,
+prompting them to enter the respective prices. Once the prices were validated (if no input,
+default to values in assingment 1) the program opens the sales.txt file, if the file doesn't
+exist in the directory, a FileNotFound error message was displayed to the user using tkinter's
+messagebox module. So once confirming the sales.txt file existed within the directory the program
+then we process each line of the sales.txt file, and form a list of lists. Finally, we then
+iterate over our lists of lists to multiply the amount of all ticket groups (adult,child,fam pass)
+by the user's price inputs and return the total revenue.
+
+Run revenue.py to experience the GUI system and calculate the total revenue based on transactions
+and input prices.
 '''
 
 import tkinter as tk;
@@ -12,10 +28,11 @@ def process_sales() -> list[list]:
   Outputs all trasnactions listed within sales.txt file cleanly into a list of lists.
   
   Args:
-    No explicit Args, instead function relies on existence of sales.txt file
+    (None): No explicit Args, instead function relies on existence of sales.txt file
   
   Returns:
-    list[list]: One list containing all transactions which are also stored in a list i.e. sales = [[1,0,0,1],[0,0,2,1]].
+    list[list]: One list containing all transactions which are also stored in a list
+                i.e. sales = [[1,0,0,1],[0,0,2,1]].
   '''
   arr = []
   try:
@@ -37,7 +54,9 @@ def process_sales() -> list[list]:
       
       return res
   except FileNotFoundError:
-    messagebox.showerror("File Error","Error: sales.txt not found. Please ensure the file is in the same directory.")
+    # pop up message, this 
+    messagebox.showerror("FileNotFoundError",'''Error: sales.txt not found. Please ensure the
+                         file is in the same directory.''')
   
 # Helper to calculate total revenue:
 def calc_total(fam_pass:float,adult:float,child:float)-> float | None :
@@ -52,10 +71,14 @@ def calc_total(fam_pass:float,adult:float,child:float)-> float | None :
   Returns:
     (float | None): Total revenue for all transactions, (None if transaction file missing).
   '''
-  
+  # call our processed sales i.e. list of lists, to iterate over:
   sales = process_sales()
+  
+  # the only way we hit this is condition is if we go to the except statement,
+  # in process_sales(), which only occurs when the file sales.txt hasn't been found.
   if not sales:
     return None
+  
   fam_p=0
   ad=0
   ch=0
@@ -68,14 +91,26 @@ def calc_total(fam_pass:float,adult:float,child:float)-> float | None :
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+# Initialising our GUI setup:
 root = tk.Tk()
+
 root.title("Pool Revenue Simulator")
+
+# Elements in window should expand, as window expands,
+# key aspect of resizeability:
 root.columnconfigure(0,weight=1)
 root.rowconfigure(0,weight=1)
+
+# Don't want default window size to be too big or small,
+# this 600x300 resolution seems to work well across all
+# devices/platforms as a default:
 root.geometry("600x330")
 
 frame = ttk.Frame(root, padding=10)
 frame.grid(column = 0, row = 0,sticky="NSEW")
+
+# We don't want the label to expand as page expands, only the entry fields (on column1)
+# should expand 3x more than column 0:
 frame.grid_columnconfigure(0, weight=1)
 frame.grid_columnconfigure(1, weight=3)
 
@@ -114,10 +149,19 @@ result_label = ttk.Label(frame,
 
 def on_submit() -> None:
   '''
-  Write up docstring..
+  Called when user clicks calculate button in GUI. Receives input entered in entry fields & calls
+  calc_total() function provided input is valid.
+  
+  Args:
+    (None) : No arguments passed explicitly to the function.
+    
+  Returns:
+    (None) : Only updates result_var label with the updated total revenue based on
+             user's input prices.
   '''
   try:
-    # either checking user has entered appropriate values else using default values from AT1, as outlined in assingment description:
+    # either checking user has entered appropriate values else using default values from AT1, as
+    # outlined in assingment description:
     fam_pass_price = float(fam_pass_entry.get() or 16.00)
     adult_price = float(adult_entry.get() or 5.00)
     child_price = float(child_entry.get() or 4.00)
@@ -139,9 +183,12 @@ def on_submit() -> None:
   except ValueError:
     messagebox.showerror("ValueError:","Error: Prices must be numeric.")
 
+# button widget placed seperately to all other widgets, as python requires
+# on_submit function to be defined before trying to call it.
 button = ttk.Button(frame,text="Calculate Revenue",command=on_submit)
 
-# placing all widgets with grid:    
+# placing all widgets with grid (without placing the labels, it'll
+# have an address in memory but won't be able to be seen):    
 label.grid(column = 0, row = 0,columnspan=2,sticky="nsew")
 prompt_label.grid(column=0,row=1,columnspan=2,pady=7)
 fam_pass_label.grid(column=0, row=2, sticky="e", padx=5)
@@ -154,36 +201,4 @@ button.grid(pady=15,column=0,row=5,columnspan=2)
 result_label.grid(column=0, row=6, columnspan=2)
 
 
-
-# main loop, run until user close window or press exit:
-def main():
-  # to do: ask user for prices for the 3 inputs..
-  pass
-
-
 root.mainloop()
-
-if __name__ == "__main__":
-  main()
-
-
-'''
-Everything done step by step... not the best implementation in terms of time and space complexity, this will suffer hard under scale,
-but built from logical principles started with this in online python interpreter. This small peice serves basis of entire prorgam:
-
-sales = [[1, 0, 0, 0],[0, 0, 2, 0],[0, 1, 1, 0],[2, 0, 0, 1]] {This is the entire purpose of process_sales functions to get our sales file data looking nice like this}
-
-def calc_total(fam_pass_a,fam_pass_b,adult,child):
-    fam_a=0
-    fam_b=0
-    ad=0
-    ch=0
-    for transaction in sales:
-        fam_a += (fam_pass_a * transaction[0])
-        fam_b += (fam_pass_b * transaction[1])
-        ad += (adult * transaction[2])
-        ch += (child * transaction[3])
-    return fam_a + fam_b + ad + ch
-    
-print(f"Total rev for this transaction: ${calc_total(16,16,5,4)}")
-'''
